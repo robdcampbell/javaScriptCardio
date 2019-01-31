db06319377e56a5f1aadba455d1463af3b6ec263
Rob Campbell <rdeancampbell@gmail.com>
2019-01-31 00:35:32 -0500

    Reversing Arrays and Strings using algorithms, built-in-methods, and exploring recursion

--- playground/_1_30.js
+++ playground/_1_30.js
@@ -1,1 +1,87 @@
 // Daily Vanilla Javascript Cardio 
+
+
+// _______ Javascript methods _________ Be able to explain these (and what data types they apply to).
+/*
+split()  :
+
+splice()  :
+
+indexOf() :
+
+push() :
+
+pop() :
+
+shift()  :
+
+unshift()  :
+
+map()  :
+
+sort()  :
+
+filter()  :
+
+charCodeAt()  :
+
+charCode()  :
+
+
+
+*/
+
+// const str = 'hello'
+// const arr = [1,2,3,4,5]
+// const newStr = str.split('');
+
+// console.log(newStr);
+
+// console.log(str);
+// let newName = ''
+
+
+// REVERSE AN ARRAY
+
+// 1.
+    // const reverse = function(arr){
+    //   let rev = [];
+    //   arr.forEach((i) => {
+    //     rev.unshift(i);
+    //   });
+    //   return rev;
+    // };
+
+// 2.
+  //const reverse = ((val) => val.reverse()); 
+
+
+//console.log(reverse(arr));
+
+
+// REVERSE A STRING: algorithm (decrementing for loop)
+const str = 'hello'
+      // const revStr = ((str) =>{
+      //   let reverse = '';
+      //   for(let i=str.length-1; i>=0; i--){
+      //     reverse += str[i];
+      //   }
+      //   return reverse;
+      // })
+
+// REVERSE A STRING: built-in methods 
+
+        // const revStr = ((str)=>{
+        //   // 1)
+        //   // const arr = Array.from(str);
+        //   // const reverse = arr.reverse();
+        //   // const done = reverse.join('');
+        //   // return done;
+          
+        //   // 2)
+        //   return str.split("").reverse().join("");
+        // });
+
+// REVERSE A STRING: Recursion
+
+console.log(revStr(str));
