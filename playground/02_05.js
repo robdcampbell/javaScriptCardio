138e7ce0dc3df84e9f4f5b1dcaf3d7f2b847bc95
Rob Campbell <rdeancampbell@gmail.com>
2019-02-05 23:21:46 -0500

    Continued practice with prototypal inheritance and classes

---
+++ playground/02_05.js
@@ -0,0 +1,51 @@
+// Continued practice with prototypal inheritance and classes
+
+// const Person = function(name, age, gender){
+//   this.name = name;
+//   this.age = age;
+//   this.gender = gender;
+// }
+
+// Person.prototype.isAdult = function(){
+//   return this.age >= 18;
+// };
+
+// const rob = new Person('Rob Campbell', 29, 'Male');
+
+// console.log(rob.isAdult());
+
+// Using a class
+
+class Person {
+  constructor(name, age, gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  isAdult(){
+    return this.age>=18;
+  }
+  listInfo(){
+    return `${this.name} is ${this.age} and is ${this.gender}`;
+  }
+}
+
+const rob = new Person('Rob', 29,'Male');
+
+console.log(rob.listInfo());
+
+
+class ProAthelete extends Person {
+  constructor(name, age, gender,team){
+    super(name, age, gender);
+    this.team = team;
+  }
+
+  listInfo(){
+    return `${this.name} plays for The ${this.team}`;
+  }
+}
+
+const gordieHowe = new ProAthelete('Gordie Howe', 90, 'Male', 'Detroit Red Wings');
+
+console.log(gordieHowe.listInfo());
