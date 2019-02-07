fe8683247f09b067cce1e374cbe42f3cb53db67c
Rob Campbell <rcampbell1@gal-057049.local>
2019-02-07 16:53:49 -0500

    02_07 JS Cardio part 2

--- playground/02_07.js
+++ playground/02_07.js
@@ -1,15 +1,15 @@
-const Person = function(name, age, gender){
-    this.name = name;
-    this. age = age;
-    this.gender = gender;
-}
+// const Person = function(name, age, gender){
+//     this.name = name;
+//     this. age = age;
+//     this.gender = gender;
+// }
 
-
-Person.prototype.getSummary = function(){
-    return `${this.name} is a ${this.age} ${this.gender}`;  
-};
+// Person.prototype.getSummary = function(){
+//     return `${this.name} is a ${this.age} ${this.gender}`;  
+// };
    
+// const rob = new Person('Rob Campbell', 29, 'Male');
 
-const rob = new Person('Rob Campbell', 29, 'Male');
 
-console.log(rob.getSummary());
+
+const str
