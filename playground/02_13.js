052334c0ab71fed1640d0ba42a06af87cc41653b
Rob Campbell <rdeancampbell@gmail.com>
2019-02-14 09:38:35 -0500

    Continued practice with recursive functions

--- playground/02_13.js
+++ playground/02_13.js
@@ -38,19 +38,52 @@ const retailType = companies.filter((company)=> company.type === 'auto');
 
         //console.log(add(3));
 
+// FACTORIAL EXAMPLE: n!
+  // essentially if "n" = 5, 5*4*3*2*1
+
+  // function factorial(n){
+  //   if(n === 1){
+  //     return 1;
+  //   } else {
+  //     return n * factorial(n-1);
+  //   }
+  // }
+
+  // console.log(factorial(3));
+
 // REVERSING A STRING RECURSIVELY
-const string= 'Hello';
+      
+    // VERSION 1:
+          // console.log(string.substr(1)+string.charAt(0));
 
-console.log(string.substr(1)+string.charAt(0));
+            // MAKES CALL STACK EXPLODE
+          // function reverseString(str){
+          //   if(str === '') {
+          //     return '';
+          //   } else {
+          //     return reverseString(string.substr(1)+string.charAt(0));
+          //   }
+          // };
 
-function reverseString(str){
-  if(str === '') {
-    return '';
-  } else {
-    return reverseString(string.substr(1)+string.charAt(0));
-  }
-};
+          // console.log(reverseString(string));
 
-console.log(reverseString(string));
+      // VERSION 2:
+
+      const string = "Hello";
+
+      // RANDOM EXAMPLE of difference between substr and substring
+      //console.log(string);
+      // console.log(string.substr(1,3));
+      // console.log(string.substring(1,3));
+
+      function reverse(str){
+        if(str.length <= 1){
+            return str;
+        } else {
+          return reverse(str.substr(1)) + str[0];
+        }
+      }
+
+      console.log(reverse('hello'));
 
 // PRACTICAL/USEFUL RECURSION
