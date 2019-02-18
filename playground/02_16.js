e43ff86bef4a6e01fb35107152ca25ca68fdce76
Rob Campbell <rdeancampbell@gmail.com>
2019-02-17 23:26:41 -0500

    Continued different ways of prototypal inheritance

--- playground/02_16.js
+++ playground/02_16.js
@@ -3,21 +3,52 @@
 
 // Prototypal inheritance practice:
 
-// 1st obj
-function Animal(name, hasWings){
+// OBJECT CONSTRUCTOR  (can use Class in ES6, but its sugar g-damnit)
+function Animal(name) {
   this.name = name;
-  this.hasWings = hasWings;
 };
-Animal.prototype.summary = ()=> {
-  console.log(`This is a ${this.name}`)
+
+Animal.prototype = {
+    constructor: Animal,
+    numLegs: 2,
+    summary: function() {
+      console.log(`This is a ${this.name}`)
+    }
 }
 
+// Will return a constructor function in console.
+//console.log(Animal);  
+
+
+// New Instance of Animal -- using 'NEW'
+const lizard = new Animal('Lizard',false);
+// console.log(lizard.summary());
+// console.log(lizard instanceof Animal)
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Demonstrating property inheritance
+for(let property in lizard){
+  if(Animal.hasOwnProperty(property)){
+      ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+};
+    // console.log(ownProps);
+    // console.log(prototypeProps);
+
+
+// New Instance of Animal -- using 'Object.create(obj)'
 function Bird(){};
-function Bear(){};
-
-
 Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
 
-Bird.name = "Bird";
+let duck = Object.create(Bird.prototype);
+//bird.prototype.constructor = bird;
+duck.name = "Duck";
 
-console.log(Animal.summary());
+console.log(duck);
+
+//console.log(Animal);
