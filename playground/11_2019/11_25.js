c40240465d65d8f542bc880165c474ec8a2352e6
Rob <Rob@srsysadmin.local>
2019-11-25 16:51:37 -0500

    11_25 JS Cardio

---
+++ playground/11_2019/11_25.js
@@ -0,0 +1,54 @@
+
+
+
+/// Things to consider/practice/burn into memory:
+    // reduce()     --- combines all array items to create one total value
+    // map()        --- returns a new array, with 'edit' applied to each array item
+    // substring()  --- 1 Arg: will return 'rest' of string from index provided in args 
+    // replace()    --- 
+    //              ---
+
+
+
+// CARDIO:
+
+// Capitalize first letter of each word in a string
+
+function capitalize(str){
+    // let strArr = str.split(' ');
+    // return strArr.map(val => val[0].toUpperCase() + val.substring(1).toLowerCase()).join(' ');
+
+       // OR 
+       return str
+         .toLowerCase()
+         .split(' ')
+         .map(val => val[0].toUpperCase() + val.substring(1))
+         .join(' ');
+
+         // or  regular expressions
+        //  return str.replace(/\b[a-z]/gi/, function(char){
+        //      return char.toUpperCase();
+        //  })
+
+}
+// console.log(capitalize('test! This is a test tEsT TEST')); 
+
+
+// Max character
+function maxCharacter(str){
+    const charMap = {};
+    str.split('').forEach(function(char){
+        if(charMap[char]){
+            charMap[char]++
+        } else {
+            charMap[char] = 1;
+        }
+    });
+    console.log(charMap);
+}
+
+
+// Call Function
+const output = maxCharacter('javascript');
+
+console.log(maxCharacter());
