e2bdfec3f6b6de32482fda49d3bcc7b1002b8344
robdcampbell <rdeancampbell@gmail.com>
2019-12-12 16:58:09 -0500

    Continue basic image gallery with getAttribute() and setAttribute()

--- playground/12_2019/gallery-start/main.js
+++ playground/12_2019/gallery-start/main.js
@@ -5,9 +5,26 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for(let i = 1; i <= 5; i++ ){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/pic${i}.jpg`);
+    thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+    newImage.onclick = function(e){
+        const newSrc = e.target.getAttribute('src');
+        displayedImage.setAttribute('src', newSrc)
+    }
+}
+
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click',function(e){
+    const btnClass = e.target.getAttribute('class');
+
+    btnClass === 'dark' ? btn.setAttribute('class', 'light') : btn.setAttribute('class', 'dark');
+
+
+
+    console.log(btn.getAttribute('class'));
+
+});
