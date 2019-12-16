1daa7365466ceb5b9f7c3eb29297524582f4374f
Rob Campbell <rdeancampbell@gmail.com>
2019-12-15 20:36:24 -0500

    OOP practice, with the THIS keyword

--- playground/12_2019/12_12&13.js
+++ playground/12_2019/12_12&13.js
@@ -1,1 +1,11 @@
-console.log('test87i876i876876');
+const person = {
+  "name" :"Stan Rogers",
+  "age" : "30",
+  "interests" : ['tech','detroit','paint'],
+  "greeting" : function(name,age){
+    return `Hello ${this.name}, your interested in ${this.interests[0]}? Cool.`
+  } 
+}
+
+
+console.log(person.greeting());
