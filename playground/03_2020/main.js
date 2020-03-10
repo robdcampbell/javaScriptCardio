df5939cfb3926a38cd9b3380e244fcd83769cbb2
robdcampbell <rdeancampbell@gmail.com>
2020-03-10 12:24:13 -0400

    JS_Cardio, # 01

--- playground/03_2020/main.js
+++ playground/03_2020/main.js
@@ -1,5 +1,49 @@
 console.log('Also a test')
 
-const string = 'Dog';
+const string = 'This is a string';
 
-console.log(string);
+
+// 01) __________________________
+
+//Reverse a string; (array methods, for/forEach, recursion)
+function reverseStr(str){
+    //Array methods
+        // let strArr = Array.from(str).reverse().join('');
+        // return strArr;
+    
+    // For/forEach
+
+        // For Loop
+            // let revStr = '';
+            // for(i = str.length-1; i >= 0 ; i--){
+            //     revStr += str[i];
+            // }
+            // return revStr;
+    
+        // forEach
+            // let strArr = Array.from(str);
+            // let newStr = '';
+            // strArr.forEach((char)=> {
+            //     newStr = char + newStr;
+            // })
+            // return newStr;
+
+    // RECURSION
+
+    } // reverseStr function 
+
+    // RECURSION REVERSE
+    const recursionReverse = function(string) {
+        if (string === "") {
+          return "";
+        } else {
+        // recursive function is calling on a sub-problem
+          return recursionReverse(string.substr(1)) + string.charAt(0);
+        }
+      }
+
+console.log(recursionReverse(string));
+//console.log(reverseStr(string));
+
+
+// 02) __________________________
