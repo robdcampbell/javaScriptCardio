213246de60befa9028cd43ada887c20cc35840df
robdcampbell <rdeancampbell@gmail.com>
2020-03-14 18:00:08 -0400

    Continue syntax refresh: Object and property re-assignment

--- playground/03_2020/syntaxPractice.js
+++ playground/03_2020/syntaxPractice.js
@@ -5,23 +5,45 @@
 // SWITCH STATEMENTS
 // I seldom use these, but wanna drive the syntax home.
 
-function switchTest(val){
-    let answer = "";
-    
-    switch(val){ 
-        case 1:
-            answer = 'First!'
-            break;
-        case 2:
-            answer = 'Second!'
-            break;
-        case 3:
-            answer = 'Third!'
-            break;
+                // function switchTest(val){
+                //     let answer = "";
+                    
+                //     switch(val){ 
+                //         case 1:
+                //             answer = 'First!'
+                //             break;
+                //         case 2:
+                //             answer = 'Second!'
+                //             break;
+                //         case 3:
+                //             answer = 'Third!'
+                //             break;
+                //         case 4:
+                //         case 5:
+                //         case 6:
+                //             answer = 'almost!'
+                //             break;
+                //         default:
+                //             answer = 'Enter 1, 2, or 3!'
+                //             break;
+                //     }
+
+                //     return answer;
+
+                // }
+
+                // console.log(switchTest(4))
+
+// OBJECT LITERAL
+
+const myPet = {
+    'Animal': 'Dog',
+    'Name' : 'Stan',
+    'Hometown': 'Detroit',
+    'greeting' : function(){
+
     }
-
-    return answer;
-
-}
-
-console.log(switchTest(2))
+};
+console.log(myPet.Animal);
+myPet.Animal = 'Turtle'; // Re-assigns the property from dog to Turtle
+console.log(myPet.Animal) // now returns turtle
