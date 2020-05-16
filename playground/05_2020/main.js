6db170248f4a85aecd5cb8ae49d1b9d6c3cfe348
robdcampbell <rdeancampbell@gmail.com>
2020-05-16 00:06:55 -0400

    JS cardio day 1 of 100

--- playground/05_2020/main.js
+++ playground/05_2020/main.js
@@ -1,1 +1,27 @@
-console.log('its working foo!')
+//console.log('testing!')
+
+function rollCall(name, age){
+  console.log(`Hello, ${name}, are you ${age} years old?`);
+};
+rollCall('Rob','Like a Million')
+
+//
+
+const name = 'Rob';
+function reverseArr(val){
+    
+    // into an array, then array methods
+        //return Array.from(val).reverse().join('');
+
+    // Looping
+        let reverse = '';
+        for(let i = 0; i < val.length; i++){
+            reverse = val[i] + reverse;
+        }
+    return reverse;
+}
+console.log(reverseArr('Pterodactyl'));
+
+//
+
+
