75a3f279d0d265044b6f8914315b7b1b7faa8280
robdcampbell <rdeancampbell@gmail.com>
2020-05-18 23:30:20 -0400

    reverse a str using recursion

--- playground/05_2020/main.js
+++ playground/05_2020/main.js
@@ -8,20 +8,47 @@ rollCall('Rob','Like a Million')
 //
 
 const name = 'Rob';
-function reverseArr(val){
+function reverseStr(val){
     
-    // into an array, then array methods
-        //return Array.from(val).reverse().join('');
+    // 1) Built-in array methods
+            //return Array.from(val).reverse().join('');
+        // OR
+            // return val.split('').reverse().join('');
 
-    // Looping
-        let reverse = '';
-        for(let i = 0; i < val.length; i++){
-            reverse = val[i] + reverse;
-        }
-    return reverse;
-}
-console.log(reverseArr('Pterodactyl'));
+    // 2) Looping
+        //let reverse = '';
+            // For loop
+                // for(let i = 0; i < val.length; i++){
+                //     reverse = val[i] + reverse;
+                // }
+        
+            // For Each loop
+                // let strArr = Array.from(val);
+                // strArr.forEach(val => reverse = val + reverse);
 
+        //return reverse;            
+    
+} // reverseStr Function 
+
+//console.log(reverseStr('Pterodactyl'));
+
+
+// 3) Recursion
+   
+    function reverseString2(str){
+            if (str === ""){
+                return "";
+            } else { 
+                return reverseString2(str.substr(1)) + str.charAt(0);
+            }
+    }
+
+    console.log(reverseString2('hello'));
+
+
+
+
+//console.log(reverseStr2('Pterodactyl'));
 //
 
 
