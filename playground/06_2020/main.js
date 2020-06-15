253f490b173ec823a458683d36066e10096a506f
robdcampbell <rdeancampbell@gmail.com>
2020-06-15 15:06:11 -0400

    Began NetNinja OOP practice

---
+++ playground/06_2020/main.js
@@ -0,0 +1,21 @@
+
+
+// OOP practice with NetNinja:
+
+/*
+https://www.youtube.com/watch?v=4l3bTDlT6ZI&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpblB7&index=2&t=0s
+*/
+
+
+// SYLLABUS:
+/*
+    - creating objects using literal notations
+     -Javascript Classes and Constructors 
+     -Inheritance
+     -Method Chaining
+     -Prototype and Prototype inheritance 
+
+*/
+
+
+// 01  - Object Literals
