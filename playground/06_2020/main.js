2cf2cd72587cf9c276e3ede187499d1467de44bc
robdcampbell <rdeancampbell@gmail.com>
2020-06-16 12:36:28 -0400

    continued practice with Class constructors, methods, method chaining, and returning THIS

--- playground/06_2020/main.js
+++ playground/06_2020/main.js
@@ -45,7 +45,7 @@ https://www.youtube.com/watch?v=4l3bTDlT6ZI&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpb
 */
 
 // OBJECT LITERALS:
-
+/*
 const userOne = {
     email: 'ryu@ninjas.com',
     name:'Ryu',
@@ -58,15 +58,15 @@ const userOne = {
         console.log(this.email,'has logged out')
     }
 }
+*/
 
-console.log(userOne.login());
+    //console.log(userOne.login());
 
 // UPDATING OBJECT LITERALS:
-
-userOne.name = 'Yoshi';
-
-console.log(userOne.name);
-
+    /*
+    userOne.name = 'Yoshi';
+    console.log(userOne.name);
+    */
 
 
 // CLASSES
@@ -80,10 +80,29 @@ class User {
     constructor(email, name){
         this.email = email;
         this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'just logged in.');
+        return this;
+    }
+    logout(){
+        console.log(this.email, 'just logged out.')
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
     }
 }
 
 let userOne = new User('ryu@ninjas.com','Ryu');
-let userOne = new User('yoshi@mariocorp.com','Yoshi');
+let userTwo = new User('yoshi@mariocorp.com','Yoshi');
 
 //https://www.youtube.com/watch?v=hy-C4NY7A_8&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpblB7&index=6
+
+// userOne.login();
+// userTwo.logout();
+
+userOne.login().updateScore().updateScore().logout();
