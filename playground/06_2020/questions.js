0fee9b65df01b692df088c928f01c7b5c80186e2
robdcampbell <rdeancampbell@gmail.com>
2020-06-22 23:30:12 -0400

    How many Vowels? question using .matchand regex

--- playground/06_2020/questions.js
+++ playground/06_2020/questions.js
@@ -2,7 +2,7 @@
 // delete all elements in an array 
 //
 
-
+/*
 const arrEx = [1,2,3,4]
 
 arrEx.length = 0;
@@ -11,3 +11,14 @@ console.log(arrEx);
 
 const [a, ...b,c] = [1,2,3,4,5,6];
 console.log(`${a}, ${b}, and ${c}`);
+*/
+
+// How Many Vowels?
+
+console.log('test');
+
+function countVowels(str) {
+	return str.split('').filter((char)=> char.match(/[aeiou]/gi)).length;
+}
+
+console.log(countVowels('feet'));
