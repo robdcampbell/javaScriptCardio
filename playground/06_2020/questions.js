f095ffe1da5e8be7f9e141e212142d6453313846
robdcampbell <rdeancampbell@gmail.com>
2020-07-14 22:37:04 -0400

    begin user-agent test practice

--- playground/06_2020/questions.js
+++ playground/06_2020/questions.js
@@ -203,8 +203,27 @@ input object and create two arrays within the object array -  one for the keys a
 //  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _	
 //Count Ones in Binary Representation of Integer
 
-function countOnes(num){
-	return num.toString(2).split('').filter(n=>n==='1').length;
+	/*
+		function countOnes(num){
+			return num.toString(2).split('').filter(n=>n==='1').length;
+		}
+
+		console.log(countOnes(999));
+	*/
+//  Detect Browser from User Agent_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _	
+
+/*
+You need to detect what browser is being used. Create a function that takes a string (browser identifier) and returns the browser name.
+
+detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36") ➞ "Google Chrome"
+
+detectBrowser("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0") ➞ "Mozilla Firefox"
+
+detectBrowser("Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko") ➞ "Internet Explorer"
+*/
+
+
+function detectBrowser(){
+	
 }
 
-console.log(countOnes(999));
