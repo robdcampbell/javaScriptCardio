19c2b01c7688d26544cadf8d48c075bd7a9a231e
robdcampbell <rdeancampbell@gmail.com>
2020-10-19 22:43:54 -0400

    OOP with using ES6 Classes

--- playground/10_2020/main_19.js
+++ playground/10_2020/main_19.js
@@ -1,32 +1,105 @@
 
 
+// Classes
+
+    /*
+        class Person {
+            constructor(firstName, lastName,dob){
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.birthday = new Date(dob);
+            }  
+            greeting(){
+                return `Hello there ${this.firstName} ${this.lastName}`;
+            }
+            calculateAge(){
+                const diff = Date.now() - this.birthday.getTime();
+                const ageDate = new Date(diff); 
+                return Math.abs(ageDate.getUTCFullYear()-1970);
+            }
+            getsMarried(newLastName){
+                this.lastName = newLastName;
+            }
+
+            static addNumbers(x,y) {
+                return x + y;
+            }
+        }
+
+        const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+        mary.getsMarried('Thompson');
+
+        console.log(mary);
+
+        console.log(Person.addNumbers(1,4));
+
+    */
+
+
+    // Sub Classes
+
+    class Person {
+        constructor(firstName, lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        greeting(){
+            return `Hello there ${this.firstName} ${this.lastName}`;
+        }
+    }
+
+    class Customer extends Person {
+        constructor(firstName, lastName, phone, membership){
+            super(firstName, lastName);
+            this.phone = phone;
+            this.membership = membership;
+        }
+    }
+    
+    // class Customer extends Person {
+    //     constructor(firstName, lastName, phone, membership){
+    //         super(firstName,lastName);
+    //         this.phone = phone;
+    //         this.membership = membership;
+    //     }
+    // }
+
+    const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
+
+    console.log(john);
+
+
+// ****************************************************
+    
+
+
 // Creating Objects using Object.create();
 
-
-   /*         
+    /*
         const personPrototypes = {
             greeting: function(){
                 return `Hello there ${this.firstName} ${this.lastName}`;
             },
-            getsMarried: function(newLastName){
-                this.lastName =  newLastName;
+            getsMarried: function(lastName){
+                this.lastName = lastName;
             }
         }
 
         const mary = Object.create(personPrototypes);
-
         mary.firstName = 'Mary';
         mary.lastName = 'Williams';
         mary.age = 30;
 
-        mary.getsMarried('Thomspson');
+        mary.getsMarried('Thompson');
+
         console.log(mary.greeting());
 
         const brad = Object.create(personPrototypes,{
             firstName: {value: 'Brad'},
             lastName: {value: 'Traversy'},
             age: {value: 36}
-        });
+        })
 
         console.log(brad);
         console.log(brad.greeting());
@@ -34,43 +107,43 @@
     */
 
 
-// Practicing from 10/15:
 
-
-    // Create a person constructor 
-        function Person(firstName, lastName, age){
+ // Practicing from 10/15:
+/*
+        // Create a person constructor 
+        function Person(firstName, lastName){
             this.firstName = firstName;
             this.lastName = lastName;
-            this.age = age;
         }
 
-    // Add Greeting prototype to Person Constuctor
-        Person.prototype.greeting = function(){
-            return `Hello ${this.firstName} ${this.lastName}!`
-        };
+        // Add Greeting prototype to Person Constuctor
+            Person.prototype.greeting = function(){
+                return `Hello ${this.firstName} ${this.lastName}!`
+            }
 
-        const pterodactyl = new Person('Ptero', 'dactyl', 69420)
-        console.log(pterodactyl.greeting());
+            const burt = new Person('Burt', 'Reynolds')
+            console.log(burt.greeting());
 
-    // Create Customer constructor 
-        function Customer(firstName, lastName, membership, cost){
+        // Create Customer constructor 
+        
+            function Customer(firstName, lastName, member, cost){
             Person.call(this, firstName, lastName);
-            this.membership = membership;
+            this.member = member;
             this.cost = cost;
         }
- 
-    //Inherit Person prototype methods
+    
+        // Inherit Person prototype methods
         Customer.prototype = Object.create(Person.prototype);
 
-        const customer1 = new Customer('Rick', 'Sanchez', 'Ultra', 42069)
-        
-        console.log(customer1.greeting());
+        // Make customer.prototype return Customer();
+            Customer.prototype.constructor = Customer;        
 
-    // Make customer.prototype return: Customer(); (instead of Person)
-        Customer.prototype.constructor = Customer; 
-    // Overwrite Person greeting with a new Customer Greeting
+        // Overwrite Person greeting with a new Customer Greeting
         Customer.prototype.greeting = function(){
-            return `${this.firstName}, I'm in you son-of-a-bitch!`;
+                return `lace up your boots, ${this.firstName} ${this.lastName}`;
         }
 
-        console.log(customer1.greeting());
+        const rick = new Customer('Rick', 'Sanchez', 'Ultra', 69420);
+
+        console.log(rick.greeting());
+    */
