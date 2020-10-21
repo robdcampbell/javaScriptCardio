6db25996ae76b949ce55561fb6891a3e3de73107
robdcampbell <rdeancampbell@gmail.com>
2020-10-21 16:18:35 -0400

    Complete External API practice with Chuck Norris Joke generator

--- playground/10_2020/main_21.js
+++ playground/10_2020/main_21.js
@@ -1,6 +1,37 @@
 // Data from an External API _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
 
+// Add Event listener
+  /*
+    document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
+    function getJokes(e){
+      const number = document.querySelector('input[type = number').value;
+      const xhr = new XMLHttpRequest();
+
+      xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true)
+      
+      xhr.onload = function(){
+        if(this.status === 200){
+          const response = JSON.parse(this.responseText);
+
+          let output = '';
+
+          if(response.type = 'success'){
+              response.value.forEach(function(joke){
+                output += `<li>${joke.joke}</li>`
+              })
+          } else {
+            output += `<li>Something went wrong...</li>`
+          }
+          document.querySelector('.jokes').innerHTML =output;
+        }
+      }
+      
+      xhr.send();
+      e.preventDefault();
+    }
+
+    */
 
 
 
