1c6898e875dca4a3499910e1c863407bfcdbbcf0
robdcampbell <rdeancampbell@gmail.com>
2020-10-30 23:52:02 -0400

    UI element insert practice. EasyHTTP Library

--- playground/10_2020/easyHTTP/sandbox.js
+++ playground/10_2020/easyHTTP/sandbox.js
@@ -1,12 +1,9 @@
 
-
-// Event listeners 
-const taskInput = document.getElementById('task-input');
+// UI Selectors
 const taskBtn = document.getElementById('task-btn');
 
 
 // Event Listeners
-
 taskBtn.addEventListener('click', addTask);
 
 
@@ -14,6 +11,16 @@ taskBtn.addEventListener('click', addTask);
 // UI functions
 
 function addTask(e){
-    console.log(taskInput.value)
+  const taskInput = document.getElementById('task-input').value;
+  let output = '';
+  if(taskInput === ''){
+    const alertMessage = document.createElement('h3');
+    alertMessage.style.color = 'red';
+    alertMessage.textContent = 'Please add a task first.';
+    document.getElementById('output').appendChild(alertMessage);
+    setTimeout(()=>document.getElementById('output').innerHTML='',3000);
+  }else {
+
+  }
   e.preventDefault();
 }
