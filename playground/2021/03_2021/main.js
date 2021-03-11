937b82b611a2f30242e882b61cba4e3522609857
robdcampbell <rdeancampbell@gmail.com>
2021-03-11 10:42:37 -0500

    Continue DOM manipulation practice in 03_2021, event delegationa and persistence to local storage.

--- playground/2021/03_2021/main.js
+++ playground/2021/03_2021/main.js
@@ -1,11 +1,38 @@
 const addNoteBtn = document.getElementById("addNoteBtn");
 const noteInput = document.getElementById("noteInput");
 const notesList = document.getElementById("notesList");
+const deleteNoteBtn = document.querySelector(".deleteNoteBtn");
 
 addNoteBtn.addEventListener("click", addNote);
+notesList.addEventListener("click", deleteNote);
 
 function addNote(e) {
   e.preventDefault();
+  const newNote = document.createElement("li");
+  const noteText = document.createElement("p");
+  const noteDelete = document.createElement("button");
 
+  newNote.className = "newNote";
+  noteText.textContent = noteInput.value;
+  noteDelete.textContent = "X";
+  noteDelete.className = "deleteNoteBtn";
+
+  newNote.appendChild(noteText);
+  newNote.appendChild(noteDelete);
+  notesList.appendChild(newNote);
+
+  noteInput.value = "";
   console.log(noteInput.value);
 }
+
+function deleteNote(e) {
+  // A little bit of event delegation:
+  e.preventDefault();
+
+  if (e.target.className === "deleteNoteBtn") {
+    console.log("delete this note buddy!");
+    s;
+  }
+}
+
+// Link up local storage....
