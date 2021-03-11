bba3bec412ba658d06810041a0a6c040ed06ebc2
robdcampbell <rdeancampbell@gmail.com>
2021-03-11 16:35:13 -0500

    March 11 jsCardio.1

--- playground/2021/03_2021/main.js
+++ playground/2021/03_2021/main.js
@@ -3,26 +3,45 @@ const noteInput = document.getElementById("noteInput");
 const notesList = document.getElementById("notesList");
 const deleteNoteBtn = document.querySelector(".deleteNoteBtn");
 
+let notesFromStorage = localStorage.getItem("notes")
+  ? localStorage.getItem("notes")
+  : [];
+
+document.onload = getNotes();
+
 addNoteBtn.addEventListener("click", addNote);
 notesList.addEventListener("click", deleteNote);
 
+function getNotes() {
+  console.log(notesFromStorage);
+  notesFromStorage = JSON.parse(localStorage.getItem("notes"));
+  console.log(notesFromStorage);
+}
+
 function addNote(e) {
   e.preventDefault();
-  const newNote = document.createElement("li");
-  const noteText = document.createElement("p");
-  const noteDelete = document.createElement("button");
 
-  newNote.className = "newNote";
-  noteText.textContent = noteInput.value;
-  noteDelete.textContent = "X";
-  noteDelete.className = "deleteNoteBtn";
+  if (noteInput.value === "") {
+    alert("Add a note please!");
+  } else {
+    const newNote = document.createElement("li");
+    const noteText = document.createElement("p");
+    const noteDelete = document.createElement("button");
 
-  newNote.appendChild(noteText);
-  newNote.appendChild(noteDelete);
-  notesList.appendChild(newNote);
+    newNote.className = "newNote";
+    noteText.textContent = noteInput.value;
+    noteDelete.textContent = "X";
+    noteDelete.className = "deleteNoteBtn";
 
-  noteInput.value = "";
-  console.log(noteInput.value);
+    newNote.appendChild(noteText);
+    newNote.appendChild(noteDelete);
+    notesList.appendChild(newNote);
+
+    localStorage.setItem("notes", JSON.stringify(noteInput.value));
+
+    noteInput.value = "";
+    console.log(noteInput.value);
+  }
 }
 
 function deleteNote(e) {
@@ -31,7 +50,7 @@ function deleteNote(e) {
 
   if (e.target.className === "deleteNoteBtn") {
     console.log("delete this note buddy!");
-    s;
+    e.target.parentElement.remove();
   }
 }
 
