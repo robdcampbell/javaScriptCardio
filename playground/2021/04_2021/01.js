6de3f95bfd78713448a2c2544274a4f825929632
robdcampbell <rdeancampbell@gmail.com>
2021-04-01 19:40:03 -0400

    April 1 localStorage cardio part 2

--- playground/2021/04_2021/01.js
+++ playground/2021/04_2021/01.js
@@ -1,16 +1,18 @@
+// NOTES_LIST WITH LOCAL STORAGE.
+
 const addBtn = document.querySelector(".add__btn");
 const noteInput = document.querySelector(".note__input");
+const notesList = document.querySelector(".notes__output");
 
 // Event Listeners:
 addBtn.addEventListener("click", addNote);
-
 // ONLOAD: with an IIFE
 document.onload = (function () {
   getItemsFromStorage();
   setNotesList();
 })();
 
-// ***  Get items from local storage
+// 1)  ***  Get items from local storage
 function getItemsFromStorage() {
   let notesStorage = localStorage.getItem("notes");
   if (!notesStorage) {
@@ -18,17 +20,36 @@ function getItemsFromStorage() {
     console.log(notesStorage.length);
     return notesStorage;
   }
-  console.log(notesStorage);
-  return notesStorage;
-}
-// ***  Set items in local storage
-function setLocalStorage(newNote) {
-  getItemsFromStorage();
-  console.log(`TEST: ${notesStorage}`);
+  return JSON.parse(notesStorage);
 }
 
-// *** Set current notes from local storage
+// 2)  *** Set current DOM notes from local storage
 function setNotesList() {
+  let notesStorage = getItemsFromStorage();
+  if (notesStorage.length === 0) {
+    console.log("NONE");
+    return;
+  }
+  let notes = notesStorage;
+  notes.forEach((note) => {
+    // CREATE EACH NOTE:
+    const li = document.createElement("li");
+    const noteText = document.createElement("p");
+    noteText.textContent = note.text;
+    const editNoteBtn = document.createElement("button");
+    editNoteBtn.textContent = "edit";
+    const deleteNoteBtn = document.createElement("button");
+    deleteNoteBtn.textContent = "delete";
+
+    li.appendChild(noteText);
+    li.appendChild(editNoteBtn);
+    li.appendChild(deleteNoteBtn);
+
+    li.id = note.id;
+    li.classList = "note__item";
+    notesList.appendChild(li);
+  });
+
   //map through all items from localstorage and output to list
 }
 
@@ -48,18 +69,14 @@ function addNote(e) {
 
   let id = Date.now();
   let text = noteInput.value;
-  let notesStorage = JSON.parse(getItemsFromStorage());
-
-  console.log(`ID:${id}, Text: ${text}`);
+  let notesStorage = getItemsFromStorage();
   notesStorage.push({ id, text });
-
   localStorage.setItem("notes", JSON.stringify(notesStorage));
+
+  // append to the DOM
 
   noteInput.value = "";
 }
-// append to the DOM
-// grab items from local stroage
-// append to the local storage set updated list of notes
 
 /*
   ========================================================
