f146559e0acabcca70fd04ee3d56a64b23347860
robdcampbell <rdeancampbell@gmail.com>
2021-09-21 16:17:01 -0400

    Continue with localstorage instructions

--- playground/2021/09_2021/main.js
+++ playground/2021/09_2021/main.js
@@ -4,18 +4,31 @@
 const outputList = document.querySelector(".output-list");
 const searchSubmitBtn = document.querySelector(".search-submit");
 const searchInput = document.querySelector(".search-input");
+let localStorageItems;
 
 searchSubmitBtn.addEventListener("click", submitSearchTerm);
 
+document.onLoad = showListItems();
+
+// 1) look for items in local storage
+// 2) If no items, set form Output to "Add an item"
+// 3) If there are items, set "localStorageItems" to the parsed array of items
+// 4) populate the list with items from "localStorageItems"
+
+function getItemsFromStorage() {
+  console.log("testsssss");
+}
+
 function submitSearchTerm(e) {
   e.preventDefault();
-  //   console.log(searchInput.value);
   addSearchTermToList(searchInput.value);
+  // Add Item to local storage
+
   searchInput.value = "";
 }
 
 function addSearchTermToList(val) {
-  console.log(`This is my search term: ${val}`);
+  // console.log(`This is my search term: ${val}`);
   const newListItem = document.createElement("li");
   newListItem.textContent = val;
   outputList.appendChild(newListItem);
