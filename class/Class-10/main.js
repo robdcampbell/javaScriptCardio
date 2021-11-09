685f7792dcf1f87bc312630e55290629a2fdeb96
robdcampbell <rdeancampbell@gmail.com>
2021-11-09 16:39:00 -0500

    Class 10

---
+++ class/Class-10/main.js
@@ -0,0 +1,29 @@
+// also - https://css.svahtml.com/7/index.html
+
+// console.log("Hello World");
+
+// For Loops
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// Loop through an array
+
+const arr = ["apples", "bananas", "oranges", "broccoli"];
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  console.log(arr[i]);
+}
+
+// Loop through a string
+const str = "Hello world!";
+
+for (let i = 0; i <= str.length - 1; i++) {
+  console.log(str[i]);
+}
+
+// declare a function, function arguments
+
+// return the function
+
+// invoke the function
