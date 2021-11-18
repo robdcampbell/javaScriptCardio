b1fad0988d561556070ecebe5d4958cb470cfbbc
robdcampbell <rdeancampbell@gmail.com>
2021-11-18 15:03:24 -0500

    Create class 11 and begin DOM manipulation lesson

--- class/Class-10/homework.js
+++ class/Class-10/homework.js
@@ -22,16 +22,14 @@ logAllValues(["string", 9, true, "Hello World"]);
 
 /////////////////////////////////////////
 // 2) Create a function that returns a new array with words longer than 5 letters.
-function longWords(arr) {
-  console.log("test");
-  let newWordsArr = [];
 
+function longWords(arr) {
+  let newWordsArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].length >= 5) {
       newWordsArr.push(arr[i]);
     }
   }
-
   return newWordsArr;
 }
 
@@ -40,6 +38,18 @@ console.log(longWords(["string", "to", "cat", "Pterodactyl", "Hello"]));
 /////////////////////////////////////////
 // 3) Create a function that returns a new, all uppercase string.
 
+function upperCaseStr(str) {
+  return str.toUpperCase();
+}
+
+console.log(upperCaseStr("HelLO WoRLd"));
+
 /////////////////////////////////////////
 // 4) Create a function that takes a number in Farenheit, and converts to number in Celcius
 // formula: (F − 32) × 5/9 = °C
+
+function toCelsius(deg) {
+  return ((deg - 32) * 5) / 9;
+}
+
+console.log(toCelsius(72));
