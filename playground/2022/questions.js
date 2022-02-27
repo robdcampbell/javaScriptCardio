be56b47c9d0aa4195977b4953324a32119e341c4
robdcampbell <rdeancampbell@gmail.com>
2022-02-26 23:18:43 -0500

    Unique characters question - javaScriptCardio

--- playground/2022/questions.js
+++ playground/2022/questions.js
@@ -29,17 +29,37 @@ console.log(revStr);
 */
 
 // Using Recursion
-for (let i = 0; i < str.length; i++) {
-  if (i === 0) {
-  } else {
-  }
-  console.log(str.substring(i));
-}
-
+/*
+    for (let i = 0; i < str.length; i++) {
+      if (i === 0) {
+      } else {
+      }
+      console.log(str.substring(i));
+    }
+*/
 //////////////////////////////////////////
 //////////////////////////////////////////
 // 2 _ reverse an array)
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-console.log(arr.reverse());
+////////////////////////////////
+// 3 ) Detect Pangram
+
+function isPangram(string) {
+  const lower = Array.from(new Set(string.toLowerCase()));
+  const filtered = lower
+    .map((char, i) => {
+      const strCode = lower.join("").toString();
+      return strCode.charCodeAt(i);
+    })
+    .filter((char) => char >= 97 && char <= 122);
+
+  if (filtered.length === 26) {
+    return true;
+  }
+  return false;
+}
+
+const panGramTest = "The quick brown fox jumps over the lazy dog";
+console.log(isPangram(panGramTest));
