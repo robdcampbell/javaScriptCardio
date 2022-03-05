cc4a0cb8548aac18ed9f3dcec0c792b521d0e1f7
robdcampbell <rdeancampbell@gmail.com>
2022-03-04 23:19:02 -0500

    JS cardio, questions

--- playground/2022/questions.js
+++ playground/2022/questions.js
@@ -215,7 +215,7 @@ function validatePIN(pin) {
   }
   return false;
 }
-console.log(validatePIN("123456"));
+// console.log(validatePIN("123456"));
 //
 
 /////////////////////////
@@ -223,17 +223,30 @@ console.log(validatePIN("123456"));
 
 function nbYear(p0, percent, aug, p) {
   // your code
-  let years = 0;
-  let population = p0;
-  while (population <= p) {
-    // console.log(population)
-    population += (population * (percent / 100))+ aug;
-    years++;
+  let years;
+  if(percent ===0){
+    years= -1;
+  } else {
+    years = 0;
   }
+  let population = p0;
+  while (Math.floor(population) < p+1) {
+    // console.log(population)
+    population += (population * (percent / 100)+ aug);
+    years++;
+    console.log(Math.floor(population))
+  }
+  return years;
+  // let years = 0;
+  // 	for (let y = 0; p0 < p; y++) {
+  //   p0 += ((p0 * (percent / 100)) + aug);
+  //   years++
+  // }
+  
   return years;
 }
 
-console.log(nbYear(1500, 5, 100, 5000));
-console.log(nbYear(1000, 2, 50, 1200));
-console.log(nbYear(1500000, 2.5, 10000, 2000000));
-console.log(nbYear(1500000, 0.25, 1000, 2000000));
+// console.log(nbYear(1500, 5, 100, 5000));
+console.log(nbYear(1000, 2, 50, 1214));
+// console.log(nbYear(1500000, 2.5, 10000, 2000000));
+// console.log(nbYear(1500000, 0, 10000, 2000000));
