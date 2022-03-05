47086ee769ca425c78d620e193e5033cd6403d33
robdcampbell <rdeancampbell@gmail.com>
2022-03-05 00:01:25 -0500

    question 12

--- playground/2022/questions.js
+++ playground/2022/questions.js
@@ -250,3 +250,51 @@ function nbYear(p0, percent, aug, p) {
 console.log(nbYear(1000, 2, 50, 1214));
 // console.log(nbYear(1500000, 2.5, 10000, 2000000));
 // console.log(nbYear(1500000, 0, 10000, 2000000));
+
+
+///////////////////////
+// 12
+
+/*
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+*/
+
+function longestConsec(strarr, k) {
+
+  if(strarr.length < k || k <=0 || strarr.length === 0){
+    return ""
+  }
+
+  let str = "";
+  
+  for(let i = 0; i <= strarr.length-k; i++){
+  
+    let consec = "";
+    
+    for(let j = 0; j < k ; j++){
+      
+      consec += strarr[i+j];
+    }
+    
+    if(consec.length > str.length){
+      str = consec;
+    }
+  
+  }
+  return str;
+}
