784fc2f11bf170707a07b30a8e1cfb1de7df9edc
robdcampbell <rdeancampbell@gmail.com>
2022-03-07 16:32:38 -0500

    Closure Practice with scope

--- playground/2022/closure.js
+++ playground/2022/closure.js
@@ -1,17 +1,33 @@
+function closPract(val) {
+  const secretPassword = "secret";
 
-
-function closPract(val){
-
-    const secretPassword = "secret"
-
-    function getPass(){
-        if(val === secretPassword){
-            return " YOU'RE IN. "
-        } else {
-            return " NOT TODAY, BUDDY. "
-        }
+  function getPass() {
+    if (val === secretPassword) {
+      return " YOU'RE IN. ";
+    } else {
+      return " NOT TODAY, BUDDY. ";
     }
-    return getPass();
+  }
+  return getPass();
 }
+// Passphrase example, closure
+// console.log(closPract("secrets"));
 
-console.log(closPract('secrets'))
+/// EXAMPLE / INTERVIEW EXAMPLE
+
+// USING VAR
+for (var i = 0; i < 3; i++) {
+  const log = () => {
+    console.log(i);
+  };
+  setTimeout(log, 100);
+}
+// Var is updated, hoisted to the parent lexical scope
+
+// Using Let
+for (let i = 0; i < 3; i++) {
+  const log = () => {
+    console.log(i);
+  };
+  setTimeout(log, 100);
+}
