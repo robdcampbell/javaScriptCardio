b4ade2e8192ef0fabcf6b1242157ec495c171e85
robdcampbell <rdeancampbell@gmail.com>
2022-03-08 21:25:36 -0500

    toString with Radix exploration

--- playground/2022/questions.js
+++ playground/2022/questions.js
@@ -247,7 +247,7 @@ function nbYear(p0, percent, aug, p) {
 }
 
 // console.log(nbYear(1500, 5, 100, 5000));
-console.log(nbYear(1000, 2, 50, 1214));
+// console.log(nbYear(1000, 2, 50, 1214));
 // console.log(nbYear(1500000, 2.5, 10000, 2000000));
 // console.log(nbYear(1500000, 0, 10000, 2000000));
 
@@ -298,3 +298,16 @@ function longestConsec(strarr, k) {
   }
   return str;
 }
+
+///////////////////////
+// 13
+
+// Create a binary from a number.
+
+function binaryTranslate(num){
+
+  return num.toString(2);
+
+}
+
+console.log(binaryTranslate(23));
