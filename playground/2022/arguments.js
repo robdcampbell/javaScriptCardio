9709842efc10f7382ce74e2a105cf6e0275b0e94
robdcampbell <rdeancampbell@gmail.com>
2022-03-19 08:37:20 -0400

    Spreading arguments, arr

--- playground/2022/arguments.js
+++ playground/2022/arguments.js
@@ -1,5 +1,10 @@
 function args(val1, val2) {
-  return new Array(arguments);
+  // Option 1
+  //  return new Array(...arguments);
+
+  // Option 2
+  const argsArr = Array.from(arguments);
+  return argsArr;
 }
 
 console.log(args("a", "b"));
