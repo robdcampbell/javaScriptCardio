5f71792b689fef0cde35deb86e14db11061160c9
robdcampbell <rdeancampbell@gmail.com>
2022-03-30 22:35:45 -0400

    loop prac

--- playground/2022/app.js
+++ playground/2022/app.js
@@ -1,4 +1,17 @@
+// test
+console.log("test");
 
+const pokemonList = [
+  "ditto",
+  "mewtwo",
+  "snorlax",
+  "scyther",
+  "drowsee",
+  "hypno",
+];
 
+// for...of loop
 
-
+for (const pokemon of pokemonList) {
+  console.log(`i caught a ${pokemon}`);
+}
