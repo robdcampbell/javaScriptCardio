499301dd98feb072ccf91b498f9c033c699c3c4d
robdcampbell <rdeancampbell@gmail.com>
2022-04-29 23:50:46 -0400

    Background edits

--- playground/2022/other_practice/kanban/js/main.js
+++ playground/2022/other_practice/kanban/js/main.js
@@ -1,5 +1,7 @@
 import KanbanAPI from "./api/kanbanAPI.js";
 
 // TESTS
-console.log(KanbanAPI.getItems(1));
+// get from LS
+// console.log(KanbanAPI.getItems(1));
+// Insert one to LS
 // console.log(KanbanAPI.insertItem(1, "Start this project"));
