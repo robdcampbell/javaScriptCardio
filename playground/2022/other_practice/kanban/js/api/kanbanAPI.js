4718c3a3cca8b09a31179420a77c9abdc257327c
robdcampbell <rdeancampbell@gmail.com>
2022-04-30 22:50:31 -0400

    UpdateItem, Kanban

--- playground/2022/other_practice/kanban/js/api/kanbanAPI.js
+++ playground/2022/other_practice/kanban/js/api/kanbanAPI.js
@@ -39,6 +39,8 @@ export default class KanbanAPI {
         }
       }
     })();
+
+    console.log(item, currentColumn);
   }
 }
 
--- playground/2022/other_practice/kanban/js/main.js
+++ playground/2022/other_practice/kanban/js/main.js
@@ -3,5 +3,8 @@ import KanbanAPI from "./api/kanbanAPI.js";
 // TESTS
 // get from LS
 // console.log(KanbanAPI.getItems(1));
+
 // Insert one to LS
 // console.log(KanbanAPI.insertItem(1, "Start this project"));
+
+KanbanAPI.updateItem(405665);
