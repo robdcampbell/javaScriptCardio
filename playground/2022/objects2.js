ee10403941a96dfd928a8ed7d6b07fdbe7052582
robdcampbell <rdeancampbell@gmail.com>
2022-07-14 22:50:50 -0400

    JS object Factory Function prac.

---
+++ playground/2022/objects2.js
@@ -0,0 +1,68 @@
+// console.log("Hello, friend");
+
+const dogFactory = function ({ name, breed, weight }) {
+  //   let { name, breed, weight } = arguments;
+  return arguments;
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+  };
+};
+const exampleDog = dogFactory("spot", "bulldog", 47);
+console.log(exampleDog);
+
+// class Dog {
+//   constructor(name, breed, weight) {
+//     (this.name = name), (this.breed = breed), (this.weight = weight);
+//   }
+//   getInfo() {
+//     return `${this.name}, ${this.breed}, ${this.weight}`;
+//   }
+// }
+
+// const spot = new Dog("Spot", "Bull Terrier", 47);
+
+// // console.log(spot.getInfo());
+
+// class Pokemon{
+//     constructor(){
+
+//     }
+//     getInfo(){
+
+//     }
+// }
+
+// WORKS:
+// Write your code here:
+
+const dogFactory = (name, breed, weight) => {
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+    get name() {
+      return this._name;
+    },
+    get breed() {
+      return this._breed;
+    },
+    get weight() {
+      return this._weight;
+    },
+    set name(newName) {
+      return (this._name = newName);
+    },
+    set breed(newBreed) {
+      return (this._breed = newBreed);
+    },
+    set weight(newWeight) {
+      return (this._weight = newWeight);
+    },
+  };
+};
+
+const spot = dogFactory("spot", "bulldog", 45);
+
+console.log(spot);
